43757d3789e5eeacbe3f67408603f51456ed3b2c
Alex Lawry-White <designalexweb@gmail.com>
2020-04-27 20:59:40 +0100

    up to date until lecture 71, pics not working

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component'
+import ShopPage from './pages/shop/shop.component'
 import './App.css';
-
-const HatsPage = (props) => {
-  console.log("HatsPage props", props)
-  return (
-    <h1>HATS PAGE</h1>
-  );
-};
 
 function App(props) {
   return (
     <div >
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/hats" component={HatsPage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
