e7a78c2f0f08b5e868d5e52439a211233d71032c
Alex Lawry-White <designalexweb@gmail.com>
2020-04-27 17:56:25 +0100

    some basic programatic routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component'
 import './App.css';
 
-function App() {
+const HatsPage = (props) => {
+  console.log("HatsPage props", props)
+  return (
+    <h1>HATS PAGE</h1>
+  );
+};
+
+function App(props) {
   return (
     <div >
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
 
 export default App;
+
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 import './menu-item.styles.scss';
 
-const menuItem = ({ title, imageUrl, size }) => (
-  <div className={`menu-item ${size}`}>
+const menuItem = ({ title, imageUrl, size, history, linkUrl, match }) => {
+  return (
+    <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)} >
 
-    <div className="background-image" style={{ backgroundImage: `url(${imageUrl})` }} />
+      <div className="background-image" style={{ backgroundImage: `url(${imageUrl})` }} />
 
-    <div className="content">
-      <h1 className="title">{title.toUpperCase()}</h1>
-      <span className="subtitle">SHOP NOW</span>
+      <div className="content">
+        <h1 className="title">{title.toUpperCase()}</h1>
+        <span className="subtitle">SHOP NOW</span>
+      </div>
+
     </div>
+  )
+};
 
-  </div>
-);
-
-export default menuItem;
+export default withRouter(menuItem);
