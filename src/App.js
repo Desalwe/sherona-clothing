41b7105fab0ee6c12a167a63bdda80ed7b846c6f
Alex Lawry-White <designalexweb@gmail.com>
2020-05-27 22:02:36 +0100

    implemented firebase utils, including ability to store authenticated users into our firebase database

--- src/App.js
+++ src/App.js
@@ -1,38 +1,45 @@
-import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
-import HomePage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shop.component';
-import Header from './components/header/header.component';
-import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import { auth } from './firebase/firebase.utils'
-import './App.css';
+import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
+import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
+import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
+import "./App.css";
 // import { render } from '@testing-library/react';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    console.log("props", props)
+    console.log("props", props);
     this.state = {
-      currentUser: null
+      currentUser: null,
     };
   }
 
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
-      console.log("user", user)
-
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth); // this is getting the userAuth object, which is returned back out of createUserProfileDocument
+        userRef.onSnapshot((snapshot) => {
+          this.setState({ currentUser: { id: snapshot.id, ...snapshot.data() } }, () => {
+            console.log("auto updated snapshot data", this.state);
+          });
+        });
+      } else {
+        this.setState({ currentUser: userAuth }); // this is basically saying, when the user logs out (so uerAuth becomes null), set the currentUser to null.
+      }
     });
-  };
+  }
 
   componentWillUnmount() {
     this.unsubscribeFromAuth();
-  };
+  }
 
   render() {
-    console.log("this.state", this.state)
+    console.log("this.state", this.state);
     return (
       <div>
         <Header currentUser={this.state.currentUser} />
@@ -44,7 +51,6 @@ class App extends Component {
       </div>
     );
   }
-};
+}
 
 export default App;
-
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,6 +1,6 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
 
 const config = {
   apiKey: "AIzaSyCKL8hyti04nZi-H5btmcbIAdb7SkfqikA",
@@ -10,16 +10,35 @@ const config = {
   storageBucket: "sherona-clothing-db.appspot.com",
   messagingSenderId: "557952818681",
   appId: "1:557952818681:web:6fec8f3996c26b0f92b128",
-  measurementId: "G-JFY9XPZT4Z"
+  measurementId: "G-JFY9XPZT4Z",
+};
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({ displayName, email, createdAt, ...additionalData });
+    } catch (error) {
+      console.log("error creating user" + error);
+    }
+  }
+  return userRef;
 };
 
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
-export const firestore = firebase.firestore()
+export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ prompt: 'select_account' });
+provider.setCustomParameters({ prompt: "select_account" });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
